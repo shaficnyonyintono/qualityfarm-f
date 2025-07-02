c18320469ae6ff7b6493d8512336539306717c21
shaficnyonyintono <shaficnyonyintono@gmail.com>
2025-07-02 19:02:36 +0300

    all featured page fix

--- qualityfarm/src/components/AllFeaturedProducts.jsx
+++ qualityfarm/src/components/AllFeaturedProducts.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { toast } from "react-toastify";
 import ProductCard from './ProductCard';
 import "react-toastify/dist/ReactToastify.css";
